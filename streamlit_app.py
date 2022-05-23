84740464ff709068e195423921c944a5087d1456
waelyay <wael.youssef@orange.com>
2022-05-23 07:59:39 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,24 @@
+import pandas
 import streamlit
+import requests
+import snowflake.connector
 
-streamlit.title("My Parents New Healthy Dinner")
+streamlit.title('My Hotel App Website')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Chicken Biryani £10.00')
+streamlit.text('Mutton Biryani £15.00')
+streamlit.text('FIsh Biryani £20.00')
+streamlit.text('Tarang Special Biryani £50.00')
+
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
