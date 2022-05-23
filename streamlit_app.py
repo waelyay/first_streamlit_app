627e5dcb8a5f7323e55eb880fa8a273c446e0408
waelyay <wael.youssef@orange.com>
2022-05-23 08:00:19 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,12 +13,3 @@ streamlit.text('FIsh Biryani £20.00')
 streamlit.text('Tarang Special Biryani £50.00')
 
 
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
-
-
-
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
