5db87efee34aa963130400527ac1efa998dfda85
waelyay <wael.youssef@orange.com>
2022-05-23 08:02:52 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,7 @@ streamlit.title('My Hotel App Website')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Chicken Biryani £10.00')
-streamlit.text('Mutton Biryani £15.00')
-streamlit.text('FIsh Biryani £20.00')
-streamlit.text('Tarang Special Biryani £50.00')
-
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
