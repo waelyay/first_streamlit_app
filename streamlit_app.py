fee798f87b090981cc77ffa03ff5585eda2843e7
waelyay <wael.youssef@orange.com>
2022-05-23 08:05:34 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
-streamlit.test('🥑🍞 Avocado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
 
